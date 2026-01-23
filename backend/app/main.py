d0aad7293325613b6725247e1557b62dd2d83277
Lightyyyyyyy <skhaseeb6445@gmail.com>
2026-01-23 22:20:37 +0530

    main updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
+
+from app.auth.routes import router as auth_router
 
 app = FastAPI()
+
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
     return {"status": "Backend running"}
+
